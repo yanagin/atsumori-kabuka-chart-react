e2a610d33dfc4b6abab621e1c51781732468f463
k-yanagihara <yanagihara@eatsmart.co.jp>
2020-10-30 10:36:06 +0900

    エラーへの対処

--- src/App.tsx
+++ src/App.tsx
@@ -2,42 +2,17 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 
 // firebase functions
-import firebase from 'firebase';
-import { fireStore } from './firebase/index'
-import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
+import firebase from './firebase';
 
 function App() {
   const [loading, setLoading] = useState(true);
-  const [users, setUsers] = useState<firebase.firestore.DocumentData[]>([]);
   const [myAccount, setMyAccount] = useState<firebase.User>();
 
-  const uiConfig = {
-    signInFlow: 'popup',
-    signInSuccessUrl: '/',
-    signInOptions: [
-      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-    ],
-  };
-
   useEffect(() => {
-    const searchUsers = async () => {
-      // Firestoreのコレクションを指定してデータ取得
-      const res = await fireStore.collection('users').get();
-      if (res.empty) return [];
-      const userList: firebase.firestore.DocumentData[] = [];
-      // DocumentData型にはmapメソッドが定義されていないため、forEachのループでデータを加工
-      res.forEach(doc => {
-        userList.push(doc.data());
-      })
-      setUsers(userList);
-    }
-
     firebase.auth().onAuthStateChanged((user) => {
       setLoading(false);
       if (!user) return;
-      //if (user.email !== process.env.REACT_APP_VALID_MAIL_ADDRESSES) return;
       setMyAccount(user);
-      searchUsers();
     });
   }, []);
 
@@ -51,12 +26,11 @@ function App() {
         ) : !myAccount ? (
           <p>
             ログインが必要です
-            <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()} />
           </p>
         ) :
-            users.map((user, index) => {
-              return <p key={index}> {user.name}</p>
-            })
+          <p>
+            ログイン済み
+          </p>
         }
       </header>
     </div>
--- src/firebase/index.js
+++ src/firebase/index.js
@@ -10,13 +10,8 @@ export const firebaseConfig = {
     storageBucket: env.REACT_APP_STORAGE_BUCKET,
     messagingSenderId: env.REACT_APP_MESSAGING_SENDER_ID,
     appId: env.REACT_APP_APP_ID,
-    measurementId: env.REACT_APP_MEASUREMENT_ID,
 };
 
-// Firebaseを紐付け、初期化
-export const firebaseApp = firebase.initializeApp(firebaseConfig);
-
-// Firestoreのインスタンス作成
-export const fireStore = firebaseApp.firestore();
+firebase.initializeApp(firebaseConfig);
 
 export default firebase;
