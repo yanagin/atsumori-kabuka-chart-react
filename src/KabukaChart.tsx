deb2d330a4d2321c84a64f5bc8d99b344d81c4d8
k-yanagihara <yanagihara@eatsmart.co.jp>
2020-10-30 17:33:56 +0900

    日付処理の不具合を修正

--- src/KabukaChart.tsx
+++ src/KabukaChart.tsx
@@ -95,15 +95,17 @@ const KabukaChart = (props: Props) => {
         if (!date) {
             date = new Date();
         }
-        let month: string = (date.getMonth() + 1).toString();
-        if (month < '10') {
-            month = '0' + month;
+        const month = date.getMonth() + 1;
+        let sMonth = month.toString();
+        if (month < 10) {
+            sMonth = '0' + month;
         }
-        let day = (date.getDate()).toString();
-        if (day < '10') {
-            day = '0' + day;
+        const day = date.getDate();
+        let sDay = day.toString(); 
+        if (day < 10) {
+            sDay = '0' + day;
         }
-        return date.getFullYear() + '-' + month + '-' + day;
+        return date.getFullYear() + '-' + sMonth + '-' + sDay;
     }
 
     return (
