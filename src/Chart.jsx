a350d2e31b701c78a1074df16de1a1ed7a6ef562
k-yanagihara <yanagihara@eatsmart.co.jp>
2020-11-09 13:07:22 +0900

    tsでエラー(2607)が発生するのでjsxへ変更

--- src/Chart.jsx
+++ src/Chart.jsx
@@ -1,13 +1,7 @@
 import * as React from "react";
 import { Line } from "react-chartjs-2";
 
-type Props = {
-  onElementsClick?: (data: any) => void;
-  data: any;
-  options: any;
-};
-
-const Chart = (props: Props) => {
+const Chart = (props) => {
   return (
     <div className='chart'>
       <Line data={props.data} options={props.options} onElementsClick={props.onElementsClick} />
